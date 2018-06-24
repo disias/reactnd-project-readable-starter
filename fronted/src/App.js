389927e8c9eb94e02eab461695df63c719048c7f
disias <diogenes.dauster@gmail.com>
2018-06-24 18:31:59 -0300

    chore: added some library

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,8 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getCategories } from "./api/readableApi";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: []
+    };
+  }
+
+  componentDidMount() {
+    getCategories().then(categories => {
+      this.setState({ categories });
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +27,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          {this.state.categories.length > 0 &&
+            this.state.categories.map((category, index) => (
+              <li key={index}>{category}</li>
+            ))}
+        </ul>
       </div>
     );
   }
