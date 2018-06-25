ba9f4e7afb9e9b53fc5096620f1cbe3fcf75cfd2
disias <diogenes.dauster@gmail.com>
2018-06-25 12:14:40 -0300

    refactor: first layout the App

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,38 +1,178 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import redux_logo from "./redux.svg";
+import react_logo from "./react.svg";
+import udacity_logo from "./udacity.svg";
+import logo from "./logo.png";
 import { getCategories } from "./api/readableApi";
+import "semantic-ui-css/semantic.min.css";
+import { Menu, Feed, Icon, Label } from "semantic-ui-react";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      categories: []
+      categories: ["all"]
     };
   }
 
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
   componentDidMount() {
     getCategories().then(categories => {
-      this.setState({ categories });
+      this.setState(prevState => ({
+        categories: prevState.categories.concat(categories)
+      }));
     });
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ul>
+      <div>
+        <Menu stackable>
+          <Menu.Item>
+            <img src={logo} alt="logo" />
+          </Menu.Item>
+
           {this.state.categories.length > 0 &&
             this.state.categories.map((category, index) => (
-              <li key={index}>{category}</li>
+              <Menu.Item
+                key={index}
+                name={category}
+                active={category === this.state.activeItem}
+                onClick={this.handleItemClick}
+              >
+                {category}
+              </Menu.Item>
             ))}
-        </ul>
+        </Menu>
+
+        <Feed size="large">
+          <Feed.Event>
+            <Feed.Label image={udacity_logo} />
+            <Feed.Content>
+              <Feed.Summary>
+                <a>Joe Henderson</a> posted on his page
+                <Feed.Date>3 days ago</Feed.Date>
+              </Feed.Summary>
+              <Feed.Extra text>
+                Ours is a life of constant reruns. We're always circling back to
+                where we'd we started, then starting all over again. Even if we
+                don't run extra laps that day, we surely will come back for more
+                of the same another day soon.
+              </Feed.Extra>
+              <Feed.Meta>
+                <Label>
+                  <Icon name="comment" />
+                  5 Comments
+                </Label>
+                <a>
+                  <Icon name="edit outline" />
+                  Edit
+                </a>
+                <a>
+                  <Icon name="delete" />
+                  Delete
+                </a>
+                <Label as="a">
+                  <a>
+                    <Icon name="plus" />
+                  </a>
+                </Label>
+                <Icon name="like" />
+                5 Likes
+                <Label as="a">
+                  <a>
+                    <Icon name="minus" />
+                  </a>
+                </Label>
+              </Feed.Meta>
+            </Feed.Content>
+          </Feed.Event>
+
+          <Feed.Event>
+            <Feed.Label image={react_logo} />
+            <Feed.Content>
+              <Feed.Summary>
+                <a>Joe Henderson</a> posted on his page
+                <Feed.Date>3 days ago</Feed.Date>
+              </Feed.Summary>
+              <Feed.Extra text>
+                Ours is a life of constant reruns. We're always circling back to
+                where we'd we started, then starting all over again. Even if we
+                don't run extra laps that day, we surely will come back for more
+                of the same another day soon.
+              </Feed.Extra>
+              <Feed.Meta>
+                <Label>
+                  <Icon name="comment" />
+                  5 Comments
+                </Label>
+                <a>
+                  <Icon name="edit outline" />
+                  Edit
+                </a>
+                <a>
+                  <Icon name="delete" />
+                  Delete
+                </a>
+                <Label as="a">
+                  <a>
+                    <Icon name="plus" />
+                  </a>
+                </Label>
+                <Icon name="like" />
+                5 Likes
+                <Label as="a">
+                  <a>
+                    <Icon name="minus" />
+                  </a>
+                </Label>
+              </Feed.Meta>
+            </Feed.Content>
+          </Feed.Event>
+
+          <Feed.Event>
+            <Feed.Label image={redux_logo} />
+            <Feed.Content>
+              <Feed.Summary>
+                <a>Joe Henderson posted on his page</a>
+                <Feed.Date>3 days ago</Feed.Date>
+              </Feed.Summary>
+              <Feed.Extra text>
+                Ours is a life of constant reruns. We're always circling back to
+                where we'd we started, then starting all over again. Even if we
+                don't run extra laps that day, we surely will come back for more
+                of the same another day soon.
+              </Feed.Extra>
+              <Feed.Meta>
+                <Label>
+                  <Icon name="comment" />
+                  5 Comments
+                </Label>
+                <a>
+                  <Icon name="edit outline" />
+                  Edit
+                </a>
+                <a>
+                  <Icon name="delete" />
+                  Delete
+                </a>
+                <Label as="a">
+                  <a>
+                    <Icon name="plus" />
+                  </a>
+                </Label>
+                <Icon name="like" />
+                5 Likes
+                <Label as="a">
+                  <a>
+                    <Icon name="minus" />
+                  </a>
+                </Label>
+              </Feed.Meta>
+            </Feed.Content>
+          </Feed.Event>
+        </Feed>
       </div>
     );
   }
